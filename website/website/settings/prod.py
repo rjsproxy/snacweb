01d30ea893f0156d152074bf3a4c789b3d2d788a
Robert Shelton <rjs@esci.org>
2015-10-06 11:26:43 +1100

    Fix form page. Set production security values.

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -1,6 +1,34 @@
 from .base import *
 
 DEBUG = False
-CSRF_COOKIE_SECURE = True
+
+# Request HTTP strict-transport-security for 7 days.
+#SECURE_HSTS_SECONDS = 604800
+# setting to 5 mins initially.
+SECURE_HSTS_SECONDS = 300
+
+# Adds the includeSubDomains tag to HSTS header.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# Set "X-Content-Type-Options: nosniff" header on all responses.
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Set "X-XSS-Protection: 1; mode=block" header on all responses.
+SECURE_BROWSER_XSS_FILTER = True
+
+# Redirect all non-HTTPS requests to HTTPS.  Apache will do this.
+SECURE_SSL_REDIRECT = True
+
+# Request browser send cookie under HTTPS.
 SESSION_COOKIE_SECURE = True
+
+# Request browser send cookie under HTTPS.
+CSRF_COOKIE_SECURE = True
+
+# Use HttpOnly flag on the CSRF cookie.
+CSRF_COOKIE_HTTPONLY = True
+
+# Block the resource from loading in a frame.
+X_FRAME_OPTIONS = 'DENY'
+
 
